5799e2a9442e30e51acff586e8f7ab184385fcac
Nicholas Oostdyk <Noostdyk@myseneca.ca>
2018-11-02 16:22:50 -0400

    Added resources. Changed some other things

--- src/com/cyNeonBlack/window/MainPanel.java
+++ src/com/cyNeonBlack/window/MainPanel.java
@@ -19,7 +19,7 @@ public class MainPanel extends JPanel {
 	public static int RES = 64;
 
 	private Timer t = new Timer();
-	private TimerTask painter;
+	private TimerTask painter, gameLoop;
 
 	public MainPanel(Frame f) {
 		frame = f;
@@ -34,18 +34,15 @@ public class MainPanel extends JPanel {
 
 		width = RES * 20;
 		height = RES * 12;
-
 		setPreferredSize(new Dimension(width, height));
 
 		scorePanel = new ScorePanel(this);
 		add(scorePanel);
-
 		towerPanel = new TowerPanel(this);
 		add(towerPanel);
-
-	}
-
-	private void Render() {
+		
+		// Map setup
+		
 
 	}
 
@@ -59,22 +56,36 @@ public class MainPanel extends JPanel {
 		g.setColor(new Color(0, 100, 0));
 		g.fillRect(0, 0, width, height);
 
-		scorePanel.render();
+	}
 
+	private void render() {
+		repaint();
 	}
 
 	private void start() {
+
+		gameLoop = new TimerTask() {
+
+			@Override
+			public void run() {
+
+				update();
+			}
+		};
 
 		painter = new TimerTask() {
 
 			@Override
 			public void run() {
 
-				repaint();
+				render();
+				scorePanel.render();
+				towerPanel.render();
 			}
 		};
 
-		t.scheduleAtFixedRate(painter, 0, Math.round(1000 / 30f));
+		t.scheduleAtFixedRate(painter, 0, Math.round(1000 / 60f));
+		t.scheduleAtFixedRate(gameLoop, 0, Math.round(1000 / 30f));
 
 	}
 
